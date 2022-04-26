9d9648ed8ee1e2230de6a72a530bc78f470f76ae
Logan Nguyen <Lgad31vn@gmail.com>
2022-04-26 16:40:09 -0400

    Updated parent attr to BGNode

--- yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/dataStructure/graph/BusinessGNode.java
+++ yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/dataStructure/graph/BusinessGNode.java
@@ -8,13 +8,29 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class BusinessGNode {
-    int attribute;
-    List<BusinessGNode> links;
+    private int attribute;
+    private List<BusinessGNode> links;
+    private BusinessGNode parent; 
 
-    boolean canReach(BusinessGNode destination) {
-        if (this == destination) return true;
-        for (BusinessGNode node : links) 
-            if (node.canReach(destination)) return true;
+    public BusinessGNode(int attribute) {
+        this.attribute = attribute;
+        links = null;
+    }
+
+    /**
+     * Reachability check
+     * @param destination
+     * @return 
+     */
+    public boolean canReach(BusinessGNode destination) {
+        if (this.attribute == destination.attribute) return true;
+        if (this.parent != null) return false;
+        for (BusinessGNode bgNode : links) {
+            bgNode.parent = this; // set parent be this node
+            if (bgNode.canReach(destination)) return true;
+        }
         return false;
     }
+
+    
 }
