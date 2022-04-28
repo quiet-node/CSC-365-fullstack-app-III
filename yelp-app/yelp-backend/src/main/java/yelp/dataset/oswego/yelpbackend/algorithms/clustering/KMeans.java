e7d4971872d34af8b12de59171bc7e587b6321af
Logan Nguyen <Lgad31vn@gmail.com>
2022-04-27 23:49:41 -0400

    Revert KMeans

--- yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/algorithms/clustering/KMeans.java
+++ yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/algorithms/clustering/KMeans.java
@@ -12,67 +12,7 @@ import yelp.dataset.oswego.yelpbackend.models.d3Models.BusinessD3RootModel;
 
 public class KMeans {
 
-     /**
-     * A function to prepare the right JSON structure for D3
-     * @param businessBtree
-     * @param k
-     * @return BusinessD3RootModel
-     */
-    public BusinessD3RootModel prepareD3(BusinessBtree businessBtree, int k) {
-        Map<String, List<BusinessModel>> clusters = initializeClusers(businessBtree, k);
-        BusinessD3RootModel d3root = new BusinessD3RootModel();
-        List<BusinessD3ChildrenModel> d3children = new ArrayList<>();
-        List<String> centroids = new ArrayList<>();
-
-        for (String centroid : clusters.keySet()) centroids.add(centroid);
-
-        // loop through centroids
-        for (String centroid : centroids) {
-            // init d3Child to hold each child
-            BusinessD3ChildrenModel d3Child = new BusinessD3ChildrenModel();
-            d3Child.setName(centroid);
-
-            
-            if (clusters.get(centroid) != null) { // make sure this cluster has any records in it
-                // init d3models to store the list of BusinessD3Model
-                List<BusinessD3Model> d3models = new ArrayList<>();
-                
-                // loop through all the businesses in each centroid
-                for (BusinessModel business : clusters.get(centroid)) {
-                    BusinessD3Model d3Model = new BusinessD3Model();
-                    double scale = Math.pow(10, 2);
-                    d3Model.setName(business.getName() + " --- " +(Double.toString(Math.round(business.getSimilarityRate()*100*scale) / scale) + "%"));
-                    d3Model.setValue((Double.toString(Math.round(business.getSimilarityRate()*100*scale) / scale) + "%")) ;
-                    d3models.add(d3Model);
-                }
-                d3Child.setChildren(d3models);
-            }
-            d3children.add(d3Child);
-        }
-        d3root.setChildren(d3children);
-
-        return d3root;
-    }
-
-    /**
-     * A function to initialize clusters.
-     * Reference: https://www.baeldung.com/java-k-means-clustering-algorithm
-     * @param businessBtree 
-     * @param k number of clusters
-     * @return clusters
-     */
-    public Map<String, List<BusinessModel>> initializeClusers(BusinessBtree businessBtree, int k) {
-        Map<String, List<BusinessModel>> clusters = new HashMap<>();
-        List<Centroid> centroids = generateRandomCentroids(businessBtree, k);
-        List<BusinessModel> records = businessBtree.findAll();
-
-        for (BusinessModel record : records) {
-                Centroid centroid = nearestCentroid(record, centroids);
-                assignToCluster(clusters, record, centroid);
-        }
-        return clusters;
-    }
-
+     
     /**
      * A function to generate centroids randomly
      * @param businessBtree
@@ -84,7 +24,7 @@ public class KMeans {
         List<BusinessModel> businessList = businessBtree.findRandomBusinesses(k);
 
         for (int i = 0; i < businessList.size(); i++) {
-            centroids.add(new Centroid( businessList.get(i).getId(), businessList.get(i).getName(), businessList.get(i).getCategories()));
+            centroids.add(new Centroid(businessList.get(i).getId(), businessList.get(i).getName(), businessList.get(i).getCategories()));
         }
         
         return centroids;
@@ -133,4 +73,64 @@ public class KMeans {
         }
     }
 
+    /**
+     * A function to initialize clusters.
+     * Reference: https://www.baeldung.com/java-k-means-clustering-algorithm
+     * @param businessBtree 
+     * @param k number of clusters
+     * @return clusters
+     */
+    public Map<String, List<BusinessModel>> initializeClusers(BusinessBtree businessBtree, int k) {
+        Map<String, List<BusinessModel>> clusters = new HashMap<>();
+        List<Centroid> centroids = generateRandomCentroids(businessBtree, k);
+        List<BusinessModel> records = businessBtree.findAll();
+
+        for (BusinessModel record : records) {
+                Centroid centroid = nearestCentroid(record, centroids);
+                assignToCluster(clusters, record, centroid);
+        }
+        return clusters;
+    }
+
+    /**
+     * A function to prepare the right JSON structure for D3
+     * @param businessBtree
+     * @param k
+     * @return BusinessD3RootModel
+     */
+    public BusinessD3RootModel prepareD3(BusinessBtree businessBtree, int k) {
+        Map<String, List<BusinessModel>> clusters = initializeClusers(businessBtree, k);
+        BusinessD3RootModel d3root = new BusinessD3RootModel();
+        List<BusinessD3ChildrenModel> d3children = new ArrayList<>();
+        List<String> centroids = new ArrayList<>();
+
+        for (String centroid : clusters.keySet()) centroids.add(centroid);
+
+        // loop through centroids
+        for (String centroid : centroids) {
+            // init d3Child to hold each child
+            BusinessD3ChildrenModel d3Child = new BusinessD3ChildrenModel();
+            d3Child.setName(centroid);
+
+            
+            if (clusters.get(centroid) != null) { // make sure this cluster has any records in it
+                // init d3models to store the list of BusinessD3Model
+                List<BusinessD3Model> d3models = new ArrayList<>();
+                
+                // loop through all the businesses in each centroid
+                for (BusinessModel business : clusters.get(centroid)) {
+                    BusinessD3Model d3Model = new BusinessD3Model();
+                    double scale = Math.pow(10, 2);
+                    d3Model.setName(business.getName() + " --- " +(Double.toString(Math.round(business.getSimilarityRate()*100*scale) / scale) + "%"));
+                    d3Model.setValue((Double.toString(Math.round(business.getSimilarityRate()*100*scale) / scale) + "%")) ;
+                    d3models.add(d3Model);
+                }
+                d3Child.setChildren(d3models);
+            }
+            d3children.add(d3Child);
+        }
+        d3root.setChildren(d3children);
+
+        return d3root;
+    }
 }
