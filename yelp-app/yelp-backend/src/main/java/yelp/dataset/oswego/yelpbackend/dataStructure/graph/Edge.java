2403525711972a2e2f922ea56b04c8865fb71474
Logan Nguyen <Lgad31vn@gmail.com>
2022-04-30 16:56:01 -0400

    Update Edge class replaced Node with int ID

--- yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/dataStructure/graph/Edge.java
+++ yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/dataStructure/graph/Edge.java
@@ -6,9 +6,9 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class Edge implements Comparable<Edge> {
-    private Node source, destination;
+    private int source, destination;
     private double weight;
     
-    public int compareTo(Edge other) {return Double.compare(weight, other.weight);}
+    public int compareTo(Edge other) {return Double.compare(this.weight, other.weight);}
 
 }
