4460e86342465e2e67eb1d4214af680c730da110
Logan Nguyen <Lgad31vn@gmail.com>
2022-04-30 17:49:29 -0400

    Init edges in Node

--- yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/data_structure/weighted_graph/WeightedNode.java
+++ yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/data_structure/weighted_graph/WeightedNode.java
@@ -1,5 +1,6 @@
 package yelp.dataset.oswego.yelpbackend.data_structure.weighted_graph;
 
+import java.util.ArrayList;
 import java.util.List;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +11,10 @@ public class WeightedNode {
     private int businessID;
     private List<WeightedEdge> edges;
 
-    public WeightedNode(int businessID) {this.businessID = businessID;}
+    public WeightedNode(int businessID) {
+        this.businessID = businessID; 
+        this.edges = new ArrayList<>();
+    }
 
     public void addEdge(WeightedEdge edge) {this.edges.add(edge);}
 }
