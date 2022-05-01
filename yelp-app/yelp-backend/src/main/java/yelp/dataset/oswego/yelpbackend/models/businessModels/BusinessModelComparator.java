824cd6cb65986677c7cb5a7d64fbe3291a44098a
Logan Nguyen <Lgad31vn@gmail.com>
2022-04-30 20:27:24 -0400

    Create BusinessModelComparator.java

---
+++ yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/models/businessModels/BusinessModelComparator.java
@@ -0,0 +1,12 @@
+package yelp.dataset.oswego.yelpbackend.models.businessModels;
+
+import java.util.Comparator;
+
+public class BusinessModelComparator implements Comparator<BusinessModel> {
+    @Override
+    public int compare(BusinessModel o1, BusinessModel o2) {
+        if (o1.getDistance() > o2.getDistance()) return 1;
+        if (o1.getDistance() < o2.getDistance()) return -1;
+        return 0;
+    }
+}
