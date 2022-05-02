56836b0deff1e383402547e10bc442ca5bee89c9
Logan Nguyen <Lgad31vn@gmail.com>
2022-05-02 18:15:08 -0400

    Refined logic

--- yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/data_structure/weighted_graph/WeightedGraph.java
+++ yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/data_structure/weighted_graph/WeightedGraph.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import lombok.Data;
+import yelp.dataset.oswego.yelpbackend.algorithms.haversine.Haversine;
 import yelp.dataset.oswego.yelpbackend.models.businessModels.BusinessModel;
-import yelp.dataset.oswego.yelpbackend.services.WeightedGraphService;
 
 @Data
 public class WeightedGraph {
@@ -23,7 +23,34 @@ public class WeightedGraph {
         requestedBusinessList.add(0, requestedBusinessModel);
         nodeList = new ArrayList<>();
         for (int i = 0; i < requestedBusinessList.size(); i++) nodeList.add(new WeightedNode((int)requestedBusinessList.get(i).getId()));
-        new WeightedGraphService().automatingAddingNodes(requestedBusinessList, nodeList);
+        automatingAddingNodes(requestedBusinessList, nodeList);
     }
 
+    /**
+     * Connect all nodes to each other. 
+     * Greedy algorithm
+     * O(n*2+2)
+     * @param weightedNodesNumber
+     * @throws IOException
+     */
+    private void automatingAddingNodes(List<BusinessModel> businessListByDistance, List<WeightedNode> nodeList) {
+        for (int i = 0; i < businessListByDistance.size(); i++) {
+            for (int j = i+1; j < businessListByDistance.size(); j++) {
+                double weight = new Haversine().calculateHaversine(businessListByDistance.get(i), businessListByDistance.get(j));
+                addWeightedEdge(nodeList, i, j, weight);
+            }
+        }
+    }
+
+    /**
+     * Added weights to the edges
+     * @param sourceID
+     * @param destinationID
+     * @param weight
+     */
+    private void addWeightedEdge(List<WeightedNode> nodeList, int sourceID, int destinationID, double weight) {
+        WeightedEdge edge = new WeightedEdge(nodeList.get(sourceID).getBusinessID(), nodeList.get(destinationID).getBusinessID(), weight);
+        nodeList.get(sourceID).addEdge(edge);
+        nodeList.get(destinationID).addEdge(edge);
+    }
 }
