f940ca58f3e0729992e0c3546d58a87f3773b386
Logan Nguyen <Lgad31vn@gmail.com>
2022-05-02 18:16:33 -0400

    Added method to find closest four for each business

--- yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/services/GraphService.java
+++ yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/services/GraphService.java
@@ -32,5 +32,33 @@ public class GraphService {
         return businessListByDistance.subList(0, 4);
     }
 
+    /**
+     * Find four geographically nearest businesses for each business
+     * @param numNode
+     * @return Map<BusinessModel, List<BusinessModel>>
+     * @throws IOException
+     */
+    public Map<BusinessModel, List<BusinessModel>> getClosestFourAll(int numNode) throws IOException {
+        BusinessBtree businessBtree = new IOService().readBtree();
+        Map<BusinessModel, List<BusinessModel>> closestFourHashMap = new HashMap<>();
+        
+        for (int i = 0; i < numNode; i++) {
+            List<BusinessModel> closestFourList = new ArrayList<>();
+            BusinessModel targetBusiness = businessBtree.findKeyByBusinessID(i);
+            for (int j = 0; j < numNode; j++) {
+                BusinessModel comparedBusiness = businessBtree.findKeyByBusinessID(j);
+                if (targetBusiness.getId() != comparedBusiness.getId()){
+                    double weight = new Haversine().calculateHaversine(targetBusiness, comparedBusiness);
+                    comparedBusiness.setDistance(weight);
+                    closestFourList.add(comparedBusiness);
+                }
+            }
+            Collections.sort(closestFourList, new BusinessModelComparator());
+            closestFourList = closestFourList.subList(0, 4);
+            closestFourHashMap.put(targetBusiness, closestFourList);
+        }
+
+        return closestFourHashMap;
+    }
     
 }
