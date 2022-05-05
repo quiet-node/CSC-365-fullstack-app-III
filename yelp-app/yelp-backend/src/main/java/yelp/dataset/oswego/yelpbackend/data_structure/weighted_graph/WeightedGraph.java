20f9595d3465b1eafe87c075da7f8bea55fb6b15
Logan Nguyen <Lgad31vn@gmail.com>
2022-05-05 17:48:31 -0400

    Update WeightedGraph.java

--- yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/data_structure/weighted_graph/WeightedGraph.java
+++ yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/data_structure/weighted_graph/WeightedGraph.java
@@ -3,11 +3,10 @@ package yelp.dataset.oswego.yelpbackend.data_structure.weighted_graph;
 import java.util.*;
 
 import lombok.Data;
-import yelp.dataset.oswego.yelpbackend.models.business_models.BusinessModel;
 
 @Data
 public class WeightedGraph {
-    private BusinessModel requestedBusinessModel;
-    private List<WeightedNode> nodeList;
-
+    private Set<WeightedNode> nodeList;
+    
+    public void addNode(WeightedNode node) {nodeList.add(node);};
 }
