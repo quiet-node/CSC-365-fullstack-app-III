a7bd9bb5650318a3ce8d35c1508e5d47db2f9d33
Logan Nguyen <Lgad31vn@gmail.com>
2022-05-06 16:20:05 -0400

    new endpoint

--- yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/controllers/BusinessController.java
+++ yelp-app/yelp-backend/src/main/java/yelp/dataset/oswego/yelpbackend/controllers/BusinessController.java
@@ -21,6 +21,7 @@ import yelp.dataset.oswego.yelpbackend.models.graph_models.connected_components.
 import yelp.dataset.oswego.yelpbackend.models.graph_models.node_models.NearestBusinessModel;
 import yelp.dataset.oswego.yelpbackend.repositories.BusinessRepository;
 import yelp.dataset.oswego.yelpbackend.services.GraphService;
+import yelp.dataset.oswego.yelpbackend.services.IOService;
 import yelp.dataset.oswego.yelpbackend.services.RestService;
 
 @RestController
@@ -69,11 +70,6 @@ public class BusinessController {
     public ResponseEntity<BusinessD3RootModel> prepareD3() throws IOException {
         return new ResponseEntity<>(new RestService().prepareD3(), HttpStatus.OK);
     }
-    
-    @GetMapping("/graph/closest/four/edges/all")
-    public ResponseEntity<List<WeightedNode>> getClosestFourEdgesForAll() throws IOException{
-        return new ResponseEntity<>(new RestService().getClosestFourNodeList(), HttpStatus.OK);
-    }
 
     @PostMapping("/graph/closest/four/edges/all/write-to-disk")
     public ResponseEntity<String> writeClosestFourByBusinessName() throws IOException{
@@ -81,14 +77,24 @@ public class BusinessController {
         return new ResponseEntity<>("Successfully write to data store",HttpStatus.OK);
     }
 
-    @GetMapping("/graph/{requestedBusiness}/closest/four")
+    @GetMapping("/graph/closest/four/{requestedBusiness}")
     public ResponseEntity<NearestBusinessModel> getClosestFourByBusinessName(@PathVariable String requestedBusiness) throws IOException{
         List<BusinessModel> businesses = repo.findByName(requestedBusiness);
         if (businesses.size() == 0) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Business Not Found");
         return new ResponseEntity<>(new RestService().getClosestFourByBusinessName(businesses.get(0)), HttpStatus.OK);
     }
 
-    @GetMapping("/graph/connectivity-check")
+    @GetMapping("/graph/closest/four/ID/{businessID}")
+    public ResponseEntity<WeightedNode> getClosestFourByBusinessID(@PathVariable Long businessID) throws IOException{
+        return new ResponseEntity<>(new RestService().getClosestFourByBusinessID(businessID), HttpStatus.OK);
+    }
+
+    @GetMapping("/graph/closest/four/edges/all")
+    public ResponseEntity<List<WeightedNode>> getClosestFourEdgesForAll() throws IOException{
+        return new ResponseEntity<>(new RestService().getClosestFourNodeList(), HttpStatus.OK);
+    }
+
+    @GetMapping("/graph/fetch/connected-components")
     public ResponseEntity<List<ConnectedComponenet>> connectivityCheck() throws IOException{
         return new ResponseEntity<>(new GraphService().fetchConnectedComponents(), HttpStatus.OK);
     }
