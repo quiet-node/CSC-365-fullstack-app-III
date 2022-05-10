87b9adcb61a6653f4fc72adca141dd969aaf2954
Logan Nguyen <Lgad31vn@gmail.com>
2022-05-10 12:34:30 -0400

    Updated Landing layout

--- yelp-app/yelp-frontend/src/pages/Landing.tsx
+++ yelp-app/yelp-frontend/src/pages/Landing.tsx
@@ -9,19 +9,25 @@ const Landing = () => {
           <Header />
         </div>
         <div className='flex justify-center items-center w-full h-36 mt-36'>
-          <div className='bg-white h-36 flex justify-center items-center w-[750px] rounded-lg drop-shadow-2xl'>
-            <div className='flex items-center'>
+          <div className='bg-white h-52 flex justify-center items-center w-[750px] rounded-lg drop-shadow-2xl'>
+            <div className='flex flex-col items-center'>
               <Link
                 to='/yelp-data'
-                className='cursor-pointer bg-indigo-500 px-10 ml-5 shadow-2xl hover:drop-shadow-lg rounded-lg py-2 font-bold text-white hover:bg-indigo-600 '
+                className='cursor-pointer w-72 bg-indigo-500 text-center ml-5 shadow-2xl hover:drop-shadow-lg rounded-lg py-2 font-bold text-white hover:bg-indigo-600 '
               >
                 Look up a business
               </Link>
               <Link
                 to='/clusters'
-                className='cursor-pointer bg-indigo-500 px-10 ml-5 shadow-2xl hover:drop-shadow-lg rounded-lg py-2 font-bold text-white hover:bg-indigo-600 '
+                className='cursor-pointer w-72 bg-indigo-500 text-center ml-5 mt-5 shadow-2xl hover:drop-shadow-lg rounded-lg py-2 font-bold text-white hover:bg-indigo-600 '
               >
                 Fetch random clusters
+              </Link>
+              <Link
+                to='/graph'
+                className='cursor-pointer w-72 bg-indigo-500 text-center ml-5 mt-5 shadow-2xl hover:drop-shadow-lg rounded-lg py-2 font-bold text-white hover:bg-indigo-600 '
+              >
+                Find shortest path
               </Link>
             </div>
           </div>
