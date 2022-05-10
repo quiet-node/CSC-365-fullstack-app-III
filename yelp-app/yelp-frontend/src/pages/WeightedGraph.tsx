b3441efb1fc29d2ee65a9da5e64320ce0ac76b7e
Logan Nguyen <Lgad31vn@gmail.com>
2022-05-10 13:51:46 -0400

    First try Graph component from rd3g

--- yelp-app/yelp-frontend/src/pages/WeightedGraph.tsx
+++ yelp-app/yelp-frontend/src/pages/WeightedGraph.tsx
@@ -4,6 +4,86 @@ import Header from '../components/Header';
 import { Graph } from 'react-d3-graph';
 
 const WeightedGraph = () => {
+  // graph payload (with minimalist structure)
+  const data = {
+    nodes: [{ id: 'Harry' }, { id: 'Sally' }, { id: 'Alice' }],
+    links: [
+      { source: 'Harry', target: 'Sally' },
+      { source: 'Harry', target: 'Alice' },
+    ],
+  };
+
+  // the graph configuration, you only need to pass down properties
+  // that you want to override, otherwise default ones will be used
+  const myConfig = {
+    nodeHighlightBehavior: true,
+    node: {
+      color: 'lightgreen',
+      size: 120,
+      highlightStrokeColor: 'blue',
+    },
+    link: {
+      highlightColor: 'lightblue',
+    },
+  };
+
+  const onClickGraph = function (event: any) {
+    window.alert('Clicked the graph background');
+  };
+
+  const onClickNode = function (nodeId: any, node: any) {
+    window.alert('Clicked node ${nodeId} in position (${node.x}, ${node.y})');
+  };
+
+  const onDoubleClickNode = function (nodeId: any, node: any) {
+    window.alert(
+      'Double clicked node ${nodeId} in position (${node.x}, ${node.y})'
+    );
+  };
+
+  const onRightClickNode = function (event: any, nodeId: any, node: any) {
+    window.alert(
+      'Right clicked node ${nodeId} in position (${node.x}, ${node.y})'
+    );
+  };
+
+  const onMouseOverNode = function (nodeId: any, node: { x: any; y: any }) {
+    window.alert(
+      `Mouse over node ${nodeId} in position (${node.x}, ${node.y})`
+    );
+  };
+
+  const onMouseOutNode = function (nodeId: any, node: { x: any; y: any }) {
+    window.alert(`Mouse out node ${nodeId} in position (${node.x}, ${node.y})`);
+  };
+
+  const onClickLink = function (source: any, target: any) {
+    window.alert(`Clicked link between ${source} and ${target}`);
+  };
+
+  const onRightClickLink = function (event: any, source: any, target: any) {
+    window.alert('Right clicked link between ${source} and ${target}');
+  };
+
+  const onMouseOverLink = function (source: any, target: any) {
+    window.alert(`Mouse over in link between ${source} and ${target}`);
+  };
+
+  const onMouseOutLink = function (source: any, target: any) {
+    window.alert(`Mouse out link between ${source} and ${target}`);
+  };
+
+  const onNodePositionChange = function (nodeId: any, x: any, y: any) {
+    window.alert(`Node ${nodeId} moved to new position x= ${x} y= ${y}`);
+  };
+
+  // Callback that's called whenever the graph is zoomed in/out
+  // @param {number} previousZoom the previous graph zoom
+  // @param {number} newZoom the new graph zoom
+  const onZoomChange = function (previousZoom: any, newZoom: any) {
+    window.alert(`Graph is now zoomed at ${newZoom} from ${previousZoom}`);
+  };
+
   const [isLoading, setIsLoading] = useState(false);
   return (
     <div className='bg-slate-200 min-h-screen'>
@@ -26,6 +106,24 @@ const WeightedGraph = () => {
               </div>
             )}
           </div>
+          <Graph
+            id='graph-id' // id is mandatory, if no id is defined rd3g will throw an error
+            data={data}
+            config={myConfig}
+            onClickGraph={onClickGraph}
+            // onClickNode={onClickNode}
+            // onDoubleClickNode={onDoubleClickNode}
+            // onRightClickNode={onRightClickNode}
+            onClickLink={onClickLink}
+            onRightClickLink={onRightClickLink}
+            // onMouseOverNode={onMouseOverNode}
+            // onMouseOutNode={onMouseOutNode}
+            onMouseOverLink={onMouseOverLink}
+            onMouseOutLink={onMouseOutLink}
+            onNodePositionChange={onNodePositionChange}
+            onZoomChange={onZoomChange}
+          />
+          ;
         </div>
       </div>
     </div>
